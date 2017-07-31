7b43b5977e9ac50006b189f916e034d30b5bdda4
mafdet <sgoyette@kerberosfoundation.com>
2017-07-31 18:31:08 +0000

    Implemented: edit & delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,6 +14,10 @@ class RecipesController < ApplicationController
     @max_length = Recipe.validators_on( :directions ).second.options[:maximum]
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
   def create
     @recipe = Recipe.new(recipe_param)
     @recipe.chef = Chef.first
@@ -25,6 +29,21 @@ class RecipesController < ApplicationController
     end
   end
   
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_param)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted successfully!"
+    redirect_to recipes_path
+  end
   
   private
   
---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @chef = Chef.create!(chefname: "Scott", email: "scott@yahoo.com")
+    @recipe = Recipe.create(name: "Cookies", chef: @chef, ingredients: "Testing", directions: "Testing")
+  end
+  
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", ingredients: "some ingredients", directions: "some directions"} }
+    assert_template 'recipes/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
+  test "successfully edit a recipe" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_ingredients = "updated recipe ingredients"
+    patch recipe_path(@recipe), params: { recipe: {name: updated_name, ingredients: updated_ingredients } }
+    assert_redirected_to @recipe
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_ingredients, @recipe.ingredients
+  end
+  
+end
