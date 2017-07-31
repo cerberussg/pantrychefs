870b2177b447c48a64ad7310d0ce03e2888e5288
mafdet <sgoyette@kerberosfoundation.com>
2017-07-31 16:08:45 +0000

    Add new recipe with TDD

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,4 +13,22 @@ class RecipesController < ApplicationController
     @maximum_length = Recipe.validators_on( :ingredients ).second.options[:maximum]
     @max_length = Recipe.validators_on( :directions ).second.options[:maximum]
   end
+  
+  def create
+    @recipe = Recipe.new(recipe_param)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+  
+  
+  private
+  
+    def recipe_param
+      params.require(:recipe).permit(:name, :ingredients, :directions)
+    end
 end
