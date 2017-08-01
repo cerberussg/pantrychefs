4f96909f9d88f39a81fa7f93b605c5cd7265267f
mafdet <sgoyette@kerberosfoundation.com>
2017-08-01 04:38:42 +0000

    Delete chef functionality

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -36,6 +36,13 @@ class ChefsController < ApplicationController
     end
   end
   
+  def destroy
+    @chef = Chef.find(params[:id])
+    @chef.destroy
+    flash[:danger] = "chef and all recipes have been deleted!"
+    redirect_to chefs_path
+  end
+  
   private
   
   def chef_params
