32603b19836b61a0d0db8f67576db9a40a482e2d
mafdet <sgoyette@kerberosfoundation.com>
2017-08-03 21:38:47 +0000

    Add ingredient model and recipe_ingredient model, many to many association

---
+++ test/models/ingredient_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class IngreddientTest < ActiveSupport::TestCase
+  
+  def setup
+    @ingredient = Ingredient.create!(name: "Chicken")
+  end
+  
+  test "should be valid" do
+    assert @ingredient.valid?
+  end
+  
+  test "name should be present" do
+    @ingredient.name = " "
+    assert_not @ingredient.valid?
+  end
+  
+  test "name should be less than 25 characters" do
+    @ingredient.name = "a" * 26
+    assert_not @ingredient.valid?
+  end
+  
+  test "name should be more than 3 characters" do
+    @ingredient.name = "a" * 2
+    assert_not @ingredient.valid?
+  end
+  
+  test "Uniqueness of the ingredient" do
+    @ingredient2 = Ingredient.new(name: "Chicken")
+    assert_not @ingredient2.valid?
+  end
+  
+end
