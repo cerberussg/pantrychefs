49c2c6a17a92e875a86a9751c1655d3553767d56
mafdet <sgoyette@kerberosfoundation.com>
2018-03-17 17:07:44 +0000

    search bar functionality

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,7 +11,7 @@ class Recipe < ApplicationRecord
   
   def self.search(term, page)
     if term
-      where('lower(name) LIKE ? OR lower(ingredients) LIKE ? OR lower(fixing.name) LIKE ?', "%#{term}%", "%#{term}%", "%#{term}%").paginate(page: page, per_page: 10)
+      where('lower(name) LIKE ? OR lower(ingredients) LIKE ?', "%#{lower(term)}%", "%#{lower(term)}%").paginate(page: page, per_page: 10)
     else
       paginate(page: page, per_page: 10)
     end
